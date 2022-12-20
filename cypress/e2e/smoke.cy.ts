004b1c6f8e84a1448d0f66fd1eb8f23189575d93
whit33y <troppout@gmail.com>
2022-12-20 14:03:00 +0100

    Cypress issue #2 next build

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -1,5 +1,8 @@
 describe('Home page', () => {
 	it('Successfully load home page', () => {
-		cy.visit('http://localhost:3000/');
+		cy.visit('/');
+		cy.contains('Strona główna');
 	});
 });
+
+export {};
