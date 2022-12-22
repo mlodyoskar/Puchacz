b084ae0ebfb1f487ba08be4dbc235739c0f16372
whit33y <troppout@gmail.com>
2022-12-22 15:50:33 +0100

    Event details- main file added

---
+++ pages/events/[id].tsx
@@ -0,0 +1,8 @@
+import { MainLayout } from 'components/layouts/MainLayout';
+import { Typography } from 'components/atoms/Typography/Typography';
+const eventDetailsPage = () => {
+	<MainLayout>
+		<Typography component="h1">Event name here</Typography>
+	</MainLayout>;
+};
+export default eventDetailsPage;
