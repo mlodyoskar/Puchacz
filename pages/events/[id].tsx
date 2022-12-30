d59bcb06390ca38ac0456fe3c133ac3c7cc7a416
whit33y <troppout@gmail.com>
2022-12-30 12:46:45 +0100

    Event details- new button and budget show

--- pages/events/[id].tsx
+++ pages/events/[id].tsx
@@ -5,30 +5,40 @@ import { Button } from 'components/atoms/Button/Button';
 import { parties } from '../api/data.json';
 import Expand from '../../public/Expand.svg';
 import Back from '../../public/Back.svg';
+import Hide from '../../public/Hide.svg';
 import Image from 'next/image';
+import { useState } from 'react';
 const EventDetailsPage = () => {
 	const router = useRouter();
 	console.log(router.query);
 	console.log(parties);
+	const [showBudget, setShowBudget] = useState(false);
+
+	function handleHide() {
+		setShowBudget(!showBudget);
+	}
 	return (
 		<MainLayout>
+			<div className="mb-2">
+				<Typography component="h1">Szczegóły wydarzenia</Typography>
+			</div>
 			<div onClick={() => router.back()}>
 				<Button size="small">
-					Powrót <Back className="h-5 w-5 text-white" aria-hidden="true" />
+					<Typography component="h5">Back</Typography>
+					<Back className="h-5 w-5 text-white" aria-hidden="true" />
 				</Button>
 			</div>
-
-			<div className="flex flex-col items-center ">
+			<div className="mb-2 flex flex-col items-center">
 				<Image
 					src="/puch.png"
-					height={1000}
-					width={1000}
+					height={800}
+					width={800}
 					alt="Parties picture"
 					className="rounded-lg"
 				/>
 			</div>
 			<div className="flex flex-col">
-				<div className="mt-2 flex flex-row justify-center ">
+				<div className="mt-2 mb-2 flex flex-row justify-center">
 					<div className=" mx-auto rounded-lg bg-blue-700 p-5 text-white">
 						<Typography component="h2">Nazwa wydarzenia:</Typography>
 						<Typography component="h1">{parties[0].name}</Typography>
@@ -50,16 +60,57 @@ const EventDetailsPage = () => {
 						</Typography>
 					</div>
 				</div>
-				<div className="mt-2 flex justify-center">
-					<Button size="small">
-						More
-						<Expand className="h-5 w-5 text-white" aria-hidden="true" />
-					</Button>
-				</div>
-				{/* <div className="mx-auto mt-2 rounded-lg bg-blue-700 p-5 text-white">
-					<Typography component="h2">Opis:</Typography>
-					<Typography component="h1">{parties[0].Description}</Typography>
-				</div> */}
+				{showBudget ? (
+					<div className="flex flex-col">
+						<div className="mt-2 mb-2 flex flex-row justify-center">
+							<div className=" mx-auto rounded-lg bg-blue-700 p-5 text-white">
+								<Typography component="h2">Ludzi na imprezie:</Typography>
+								<Typography component="h1">
+									{parties[0].budget.people_in_party}
+								</Typography>
+							</div>
+							<div className="mx-auto rounded-lg bg-blue-700 p-5 text-white">
+								<Typography component="h2">Zarobki:</Typography>
+								<Typography component="h1">
+									{parties[0].budget.party_income}
+								</Typography>
+							</div>
+							<div className="mx-auto rounded-lg bg-blue-700 p-5 text-white">
+								<Typography component="h2">Wydatki:</Typography>
+								<Typography component="h1">
+									{parties[0].budget.party_spend}
+								</Typography>
+							</div>
+							<div className="mx-auto rounded-lg bg-blue-700 p-5 text-white">
+								<Typography component="h2">Zysk ogółem:</Typography>
+								<Typography component="h1">
+									{parties[0].budget.overall}
+								</Typography>
+							</div>
+						</div>
+						<div className="mt-2 flex justify-center">
+							<div onClick={() => setShowBudget(!showBudget)}>
+								<Button size="small">
+									<Typography component="h5">Hide</Typography>
+									<Hide className="h-5 w-5 text-white" aria-hidden="true" />
+								</Button>
+							</div>
+						</div>
+					</div>
+				) : (
+					<div className="mt-2 flex justify-center">
+						<div
+							onClick={() => {
+								handleHide();
+							}}
+						>
+							<Button size="small">
+								<Typography component="h5">More</Typography>
+								<Expand className="h-5 w-5 text-white" aria-hidden="true" />
+							</Button>
+						</div>
+					</div>
+				)}
 			</div>
 		</MainLayout>
 	);
