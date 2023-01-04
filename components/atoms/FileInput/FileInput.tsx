ad601f621793aed999e1304991575cff42c54509
Oskar Puchalski <oskarpuchalski17@gmail.com>
2023-01-04 21:20:32 +0100

    feat: added file input and schema

---
+++ components/atoms/FileInput/FileInput.tsx
@@ -0,0 +1,43 @@
+import { forwardRef } from 'react';
+
+export const FileInput = forwardRef<HTMLInputElement>((props, ref) => {
+	return (
+		<div className="flex w-full items-center justify-center">
+			<label
+				htmlFor="dropzone-file"
+				className="flex h-64 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300   bg-gray-50 hover:bg-gray-100"
+			>
+				<div className="flex flex-col items-center justify-center pt-5 pb-6">
+					<svg
+						aria-hidden="true"
+						className="mb-3 h-10 w-10 text-gray-400"
+						fill="none"
+						stroke="currentColor"
+						viewBox="0 0 24 24"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="2"
+							d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+						></path>
+					</svg>
+					<p className="mb-2 text-sm text-gray-500 ">
+						<span className="font-semibold">Kliknij, aby dodać zdjęcie</span>
+					</p>
+					<p className="text-xs text-gray-500 ">PNG, JPG (MAX. 30MB)</p>
+				</div>
+				<input
+					{...props}
+					ref={ref}
+					id="dropzone-file"
+					type="file"
+					className="hidden"
+				/>
+			</label>
+		</div>
+	);
+});
+
+FileInput.displayName = 'FileInput';
---
+++ utils/imageSchema.ts
@@ -0,0 +1,29 @@
+import { z } from 'zod';
+
+const MB_BYTES = 1000000;
+const ACCEPTED_MIME_TYPES = ['image/gif', 'image/jpeg', 'image/png'];
+
+const imageSchema = z.instanceof(FileList).superRefine((f, ctx) => {
+	if (!ACCEPTED_MIME_TYPES.includes(f[0].type)) {
+		ctx.addIssue({
+			code: z.ZodIssueCode.custom,
+			message: `File must be one of [${ACCEPTED_MIME_TYPES.join(
+				', '
+			)}] but was ${f[0].type}`,
+		});
+	}
+
+	if (f[0].size > 3 * MB_BYTES) {
+		ctx.addIssue({
+			code: z.ZodIssueCode.too_big,
+			type: 'array',
+			message: `The file must not be larger than ${3 * MB_BYTES} bytes: ${
+				f[0].size
+			}`,
+			maximum: 3 * MB_BYTES,
+			inclusive: true,
+		});
+	}
+});
+
+export { imageSchema };
