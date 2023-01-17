e3500072c263eba1b0f5befb348c42886ed2e1f1
Oskar Puchalski <oskarpuchalski17@gmail.com>
2023-01-17 20:38:26 +0100

    fix: new sidebar and bavigation look

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -1,34 +1,9 @@
-import cx from 'clsx';
-import { Hamburger } from 'components/atoms/Hamburger/Hamburger';
 import { Sidebar } from 'components/molecules/Sidebar/Sidebar';
-import React, { useState } from 'react';
 
 interface Props {
 	children: React.ReactNode;
 }
 
 export const MainLayout = ({ children }: Props) => {
-	const [isOpen, setIsOpen] = useState(false);
-
-	const toggleIsOpen = () => setIsOpen((prevValue) => !prevValue);
-
-	return (
-		<div>
-			<div
-				className={cx(
-					'md:right- absolute right-0  left-0 transition-transform md:fixed md:right-full md:translate-x-0',
-					{
-						'translate-x-0': isOpen,
-						'-translate-x-full': !isOpen,
-					}
-				)}
-			>
-				<Sidebar />
-			</div>
-			<div className="fixed z-20 flex w-full justify-end p-4 md:hidden">
-				<Hamburger isOpen={isOpen} toggleIsOpen={toggleIsOpen} />
-			</div>
-			<main className="p-4 md:ml-64">{children}</main>
-		</div>
-	);
+	return <Sidebar>{children}</Sidebar>;
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Input } from 'components/atoms/Input/Input';
 import { Typography } from 'components/atoms/Typography/Typography';
 import { MainLayout } from 'components/layouts/MainLayout';
 
@@ -5,6 +6,20 @@ const HomePage = () => {
 	return (
 		<MainLayout>
 			<Typography component="h1">Strona główna</Typography>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic sed
+				maiores fuga consectetur voluptates nemo, facilis ea accusamus, itaque
+				earum laborum, nobis incidunt. Sint sapiente beatae nostrum alias
+				pariatur, similique aliquid. Architecto debitis cupiditate explicabo
+				officiis ipsum asperiores sunt labore eveniet adipisci maxime totam,
+				nisi ipsam sint nemo fuga nesciunt dignissimos voluptatem maiores! Magni
+				praesentium, modi unde maxime adipisci temporibus perferendis nostrum
+				nesciunt aut, culpa sunt ut debitis aliquid alias quidem consequuntur
+				earum illo quas? Quae quam, molestias qui quod, numquam at neque
+				deserunt ducimus quidem sed ex nisi dolor possimus amet omnis quia
+				tenetur quis. Perferendis libero ex hic.
+			</p>
+			<Input> Co jest nie ma zamulania</Input>
 		</MainLayout>
 	);
 };
