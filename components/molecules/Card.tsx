cf55d2f63cfbf009d1eb80af9e03f9199d79255a
Oskar Puchalski <oskarpuchalski17@gmail.com>
2023-01-18 09:20:42 +0100

    feat: basic statistics page

---
+++ components/molecules/Card.tsx
@@ -0,0 +1,46 @@
+import Link from 'next/link';
+
+interface Props {
+	name: string;
+	amount: string;
+	icon: any;
+	href?: string;
+}
+
+export const Card = ({ name, amount, icon: Icon, href }: Props) => {
+	return (
+		<div key={name} className="overflow-hidden rounded-lg bg-white shadow">
+			<div className="p-5">
+				<div className="flex items-center">
+					<div className="flex-shrink-0">
+						<Icon className="h-6 w-6 text-gray-400" aria-hidden="true" />
+					</div>
+					<div className="ml-5 w-0 flex-1">
+						<dl>
+							<dt className="truncate text-sm font-medium text-gray-500">
+								{name}
+							</dt>
+							<dd>
+								<div className="text-lg font-medium text-gray-900">
+									{amount}
+								</div>
+							</dd>
+						</dl>
+					</div>
+				</div>
+			</div>
+			{href && (
+				<div className="bg-gray-50 px-5 py-3">
+					<div className="text-sm">
+						<Link
+							href={href}
+							className="font-medium text-blue-700 hover:text-blue-900"
+						>
+							Szczegóły
+						</Link>
+					</div>
+				</div>
+			)}
+		</div>
+	);
+};
--- pages/statistics.tsx
+++ pages/statistics.tsx
@@ -1,7 +1,42 @@
 import { Typography } from 'components/atoms/Typography/Typography';
+import { Card } from 'components/molecules/Card';
+import UserIcon from './../components/icons/User.svg';
+import DollarIcon from './../components/icons/Dollar.svg';
+import UserGroupIcon from './../components/icons/UserGroup.svg';
+
+const cards = [
+	{ name: 'Stan konta', href: '#', icon: DollarIcon, amount: '12 496 PLN' },
+	{
+		name: 'Osób na ostatniej imprezie',
+		href: '#',
+		icon: UserIcon,
+		amount: '378',
+	},
+	{ name: 'Osób łącznie', href: '#', icon: UserGroupIcon, amount: '20 432' },
+	{ name: 'Ilość imprez', href: '#', icon: UserIcon, amount: '13' },
+];
 
 const StatisticsPage = () => {
-	return <Typography component="h1">Logs Statistics page</Typography>;
+	return (
+		<>
+			<div className="min-h-full">
+				<div className="flex flex-1 flex-col">
+					<main className="flex-1 pb-8">
+						<div className="">
+							<div className="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8">
+								<Typography component="h2">Podsumowanie</Typography>
+								<div className="mt-2 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+									{cards.map((card) => (
+										<Card key={card.name} {...card} />
+									))}
+								</div>
+							</div>
+						</div>
+					</main>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default StatisticsPage;
