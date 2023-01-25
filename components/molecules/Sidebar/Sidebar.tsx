ba1248e3d5538b79ea5febe710f23b4752ec401d
Oskar Puchalski <oskarpuchalski17@gmail.com>
2023-01-25 14:10:52 +0100

    fix: fixed unused code and ts problem

--- components/molecules/Sidebar/Sidebar.tsx
+++ components/molecules/Sidebar/Sidebar.tsx
@@ -229,31 +229,19 @@ export const Sidebar = ({ children }: Props) => {
 										leaveTo="transform opacity-0 scale-95"
 									>
 										<Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-											{userNavigation.map(({ name, href, onClick }) => (
+											{userNavigation.map(({ name, onClick }) => (
 												<Menu.Item key={name}>
-													{({ active }) =>
-														href ? (
-															<Link
-																href={href}
-																className={clsx(
-																	active ? 'bg-gray-100' : '',
-																	'block px-4 py-2 text-sm text-gray-700'
-																)}
-															>
-																{name}
-															</Link>
-														) : (
-															<button
-																onClick={onClick}
-																className={clsx(
-																	active ? 'bg-gray-100' : '',
-																	'w-full px-4 py-2 text-start text-sm text-gray-700'
-																)}
-															>
-																{name}
-															</button>
-														)
-													}
+													{({ active }) => (
+														<button
+															onClick={onClick}
+															className={clsx(
+																active ? 'bg-gray-100' : '',
+																'w-full px-4 py-2 text-start text-sm text-gray-700'
+															)}
+														>
+															{name}
+														</button>
+													)}
 												</Menu.Item>
 											))}
 										</Menu.Items>
